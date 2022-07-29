2d2c9e18d26825aaca80bd5681c92e816eed494f
stefansnaerh <stefansnaerh@gmail.com>
2022-07-29 10:45:34 +0000

    random movie generator almost ready. Just needs some better styling otherwise complete

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ import {config} from "./config.js"
 const BASE_URL = config.api_base_url
 const API_KEY = config.api_key
 const API_RANDOM_OVER_7 = config.api_over_7
+const IMAGE_URL = config.image_base_url
 
 
 // getting a random page number since it always starts at page 1
@@ -11,52 +12,70 @@ let PAGE_NUMBER = Math.floor(Math.random() * 500)
 console.log(PAGE_NUMBER)
 
 
-// function that gets all movies after 1980 with over 7 ratings
-// next need to map over the array and pick one movie
-/*
+
 async function getMovie() {
-    let response = await fetch(`${BASE_URL}discover/movie?api_key=${API_KEY}&language=en-US&page=${PAGE_NUMBER}&${API_RANDOM}`);
-    let data = await response.json()
-    return data;
-}
-getMovie().then(data => console.log(data));
-
-
-*/
-
-/*
-async function getMovie() {
-
+    // Getting a random number from 1-20 to get random movie from the API array
     let randomMovie = Math.floor(Math.random() * 20)
-
-    let data = []
-    try {
-    let response = await fetch(`${BASE_URL}discover/movie?api_key=${API_KEY}&language=en-US&page=${PAGE_NUMBER}&${API_RANDOM_OVER_7}`);
-    let responseData = await response.json()
-    data = responseData?.results[randomMovie]
-
-    } catch (error) {
-
-    }
-    return data;
-}
-getMovie().then(data => console.log(data));
-
-*/
-async function getMovie() {
-
-    let randomMovie = Math.floor(Math.random() * 20)
+    // Connecting to the API
     let response = await fetch(`${BASE_URL}discover/movie?api_key=${API_KEY}&language=en-US&page=${PAGE_NUMBER}&${API_RANDOM_OVER_7}`);
     let responseData = await response.json()
     console.log(responseData)
 
+    // Manipulating the HTML - adding and removing elements when the function is activated
+    document.getElementById("quality-text-container").style.display = "none"
+    document.getElementById("random-movie-container").style.display = "initial"
+    document.getElementById("movie-card-headline").style.display = "none"
+    document.getElementById("random-movie-button").innerHTML = "Next!" 
+    document.getElementById("random-movie-button").style.marginTop = "0"   
+
+
+    // When button is clicked, append the info from the api to the html file
     const movie = responseData.results[randomMovie]
     let movieTitle = movie.title
     document.getElementById("movie-title").innerHTML = movieTitle
+    let moviePoster = movie.poster_path
+    document.getElementById("movie-poster").src = `${IMAGE_URL}${moviePoster}`
+    let movieDate = movie.release_date.slice(-10, -6)
+    document.getElementById("release-date").innerHTML = movieDate
+    let movieRating = movie.vote_average
+    document.getElementById("movie-rating").innerHTML = movieRating
+    let movieOverview = movie.overview
+    const overviewP = document.getElementById("overview-sub")
+    overviewP.innerHTML = movieOverview
+
+    // If the overview text is longer then 120 letters, add a read more button
+    if (movieOverview.length > 120 ) {
+        overviewP.innerHTML =  `${movieOverview.slice(0, 120)}`
+
+        const readMore = document.getElementById("read-more")
+        readMore.innerHTML = "...read all"
+
+        function readMoreButton() {
+            overviewP.innerHTML = movieOverview
+            readMore.style.display = "none"
+
+        
+            
+            // when readmore is clicked, change to read less
+            const readLess = document.getElementById("read-less")
+            readLess.innerHTML = "read less"
+
+            readLess.addEventListener("click", readLessButton)
+
+            function readLessButton () {
+                overviewP.innerHTML =  `${movieOverview.slice(0, 120)}`
+                readLess.style.display = "none"
+                readMore.style.display = "initial"
+            }
+        }
+        readMore.addEventListener("click", readMoreButton)
+
+    }
+
+
 
    
 }
-getMovie()
 
 
 
